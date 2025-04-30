797f7106123ce97e4783a88ff23ceb5d64671c40
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-04-30 17:08:56 +0900

    feat: 컴포넌트 상태관리

--- week5/lyouxsun/src/SearchBar.js
+++ week5/lyouxsun/src/SearchBar.js
@@ -1,15 +1,38 @@
 import React from "react";
 
-const SearchBar = () => {
-    return (
-        <form>
-           <input type={"text"} placeholder="Search..."/>    
-           <label>
-            <input type={"checkbox"}/>
-            Only show products in stock
-           </label>
-        </form>
-        );
-}
+const SearchBar = ({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange,
+}) => {
+
+    const handleTextChange = (e) => {
+        console.log(e.target.value);
+        onFilterTextChange(e.target.value);
+    }
+
+    const handleCheckboxChange = (e) => {
+        onInStockOnlyChange(e.target.checked);
+    }
+  return (
+    <form>
+      <input
+        type={"text"}
+        value={filterText}
+        placeholder="Search..."
+        onChange = {handleTextChange}
+      />
+      <label>
+        <input
+          type={"checkbox"}
+          checked={inStockOnly}
+          onChange={handleCheckboxChange}
+        />
+        Only show products in stock
+      </label>
+    </form>
+  );
+};
 
 export default SearchBar;
