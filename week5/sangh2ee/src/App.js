873cabc8d38e1ffa53faf6c39719ae3181f174c1
sangh2ee <sangh2ee10@gmail.com>
2025-05-04 07:56:05 +0900

    feat : Delete 구현현

--- week5/sangh2ee/src/App.js
+++ week5/sangh2ee/src/App.js
@@ -1,8 +1,20 @@
- 
+import FilterableProductTable from "./FilterableProductTable";
+import {useState} from "react";
+
+
 const App=()=> {
+  const[products, setProducts] = useState([
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+
+  ])
   return (
-    <div className="App">hello world world</div>    
-  );
+  <FilterableProductTable products={products} setProducts={setProducts}/>
+);
 }
 
 export default App;
---
+++ week5/sangh2ee/src/ProductTable.js
@@ -0,0 +1,51 @@
+import React , { useState }from 'react'; 
+import ProductCategoryRow from './ProductCategoryRow';
+import ProductRow from './ProductRow';
+import Deleteproduct from './Deleteproduct';
+
+const ProductTable = ({ products,filterText, inStockOnly,onDelete }) => {
+    const rows=[];
+    let lastCategory=null;
+
+    products.map((product) => {
+        if(product.name.toLowerCase().indexOf(filterText.toLowerCase()) == -1){
+            return;
+        }
+
+        if(inStockOnly&&!product.stocked){
+            return;
+        }
+
+        if (product.category !== lastCategory){
+            rows.push (
+                <ProductCategoryRow
+                category={product.category}
+                key={product.category}
+                onDelete={onDelete}/> 
+            );
+        }    
+    rows.push(<ProductRow 
+      product={product} 
+      key={product.name}
+      onDelete={onDelete}
+      />);
+      lastCategory=product.category;
+      });
+      
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+           {rows}
+        </tbody>
+      </table>
+    );
+  };
+  
+  export default ProductTable;
