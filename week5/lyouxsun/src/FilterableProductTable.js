c2b1af6113c74734fa7d4e53c905b38f7492ad38
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-05-04 15:41:47 +0900

    feat: 상품 삭제 기능 구현

--- week5/lyouxsun/src/FilterableProductTable.js
+++ week5/lyouxsun/src/FilterableProductTable.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import SearchBar from "./SearchBar";
+import InputBar from "./InputBar";
 import ProductTable from "./ProductTable";
 
-const FilterableProductTable = ({ products }) => {
+const FilterableProductTable = ({ products, setProducts }) => {
   const [filterText, setFilterText] = useState("");
   const [inStockOnly, setInStockOnly] = useState(false);
+
+  const addProduct = (product) => {
+    setProducts((products) => [...products, product]);    // products 배열 데이터 변경 (데이터 추가)
+  };
+
+  const deleteProduct = (deleted) => {
+    console.log(deleted);
+    setProducts(products.filter(product => product !== deleted));    // products 배열 데이터 변경 (데이터 추가)
+  };
+
   return (
     <div>
       <SearchBar
@@ -17,7 +28,9 @@ const FilterableProductTable = ({ products }) => {
         products={products}
         filterText={filterText}
         inStockOnly={inStockOnly}
+        deleteProduct={deleteProduct}
       />
+      <InputBar addProduct={addProduct} />
     </div>
   );
 };
