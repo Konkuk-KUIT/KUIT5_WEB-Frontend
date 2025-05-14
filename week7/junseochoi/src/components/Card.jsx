4d91b36089a89f9e5c9a71b18a1fba3755f9bd31
[evan7484] <[evan7484@gmail.com]>
2025-05-14 10:49:12 +0900

    카드 맞추기 구현 완료료

--- week7/junseochoi/src/components/Card.jsx
+++ week7/junseochoi/src/components/Card.jsx
@@ -5,37 +5,41 @@ import * as C from "./Card.Styled";
 
 const Card = ({ count, setCount }) => {
   const [flipped, setFlipped] = useState(Array(CardList.length).fill(false));
+  const [selected, setSelected] = useState([]);
 
-  // 뒤집혀진 카드들 중 쌍이 맞는 카드가 있는지 확인인
+  // 뒤집혀진 카드들 중 쌍이 맞는 카드가 있는지 확인
   useEffect(() => {
-    const frontCount = {};
+    if (selected.length === 2) {
+      const [firstIndex, secondIndex] = selected;
+      const firstCard = CardList[firstIndex];
+      const secondCard = CardList[secondIndex];
 
-    CardList.forEach((item, idx) => {
-      if (flipped[idx]) {
-        frontCount[item.front] = (frontCount[item.front] || 0) + 1;
+      if (firstCard.front === secondCard.front) {
+        setCount((prev) => prev + 1);
+        setSelected([]);
+      } else {
+        const timer = setTimeout(() => {
+          const updatedFlipped = [...flipped];
+          updatedFlipped[firstIndex] = false;
+          updatedFlipped[secondIndex] = false;
+          setFlipped(updatedFlipped);
+          setSelected([]);
+        }, 3000);
+
+        return () => clearTimeout(timer);
       }
-    });
-
-    const countPairs = Object.values(frontCount).filter(
-      (cnt) => cnt >= 2
-    ).length;
-
-    setCount(countPairs);
+    }
   });
 
   // 쌍이 맞는 카드가 있다면 onClick이 작동하지 않음
   const handleClick = (index) => {
-    const currentFront = CardList[index].front;
-
-    const CompareFlipped = CardList.filter(
-      (item, idx) => item.front === currentFront && flipped[idx]
-    ).length;
-
-    if (CompareFlipped >= 2) return;
+    if (flipped[index]) return; // 앞면에서 뒷면으로 뒤집기 예외처리
+    if (selected.length >= 2) return; // 2개 이상 뒤집기 불가
 
     const updatedFlipped = [...flipped];
     updatedFlipped[index] = !updatedFlipped[index];
     setFlipped(updatedFlipped);
+    setSelected([...selected, index]);
   };
   return (
     <>
