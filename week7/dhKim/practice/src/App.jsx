389c149e72d588e0d66ce56cffe4ce148778f36b
KIM-korea <dohyun3885@gmail.com>
2025-05-16 17:36:17 +0900

    chore: 실습 완료료

---
+++ week7/dhKim/practice/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Search from "./components/Search";
+import {KUIT_WEB} from "./data";
+import Card from "./components/Card";
+import useSearch from "./hooks/useSearch";
+function App(){
+  const {search, filter, handleSearch,handleBtn}=
+  useSearch(KUIT_WEB);
+  
+  return(
+    <>
+      <Search search={search}
+      handleSearch={handleSearch}
+      handleBtn={handleBtn}/>
+      <section
+      style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          padding: "20px",
+        }}>
+        
+        {
+            filter
+            .map((member)=><Card 
+            name={member.name} 
+            age={member.age}/>)
+        }
+        </section>
+    </>
+  );
+}
+
+export default App;
