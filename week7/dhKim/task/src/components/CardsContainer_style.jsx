edd4310afb6b4a99483838645f28bebf4f25eea7
KIM-korea <dohyun3885@gmail.com>
2025-05-16 23:40:18 +0900

    chore: card display change

--- week7/dhKim/task/src/components/CardsContainer_style.jsx
+++ week7/dhKim/task/src/components/CardsContainer_style.jsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const CardsContainerStyled=styled.div`
-    width:100%;
-    height:100%;
-
-    display: grid;
-    grid-template-columns: repeat (5,1fr);
-    gap: 10px;
+  
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap:20px;
+    padding: 20px;
     
 `;
