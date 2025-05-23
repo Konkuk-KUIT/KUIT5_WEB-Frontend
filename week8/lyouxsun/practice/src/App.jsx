c43feca636a3d33f9b65d5dcf64e32ac99bcdb3a
lyouxsun <leedrkr323@naver.com>
2025-05-23 17:30:46 +0900

    feat: 8주차 실습

---
+++ week8/lyouxsun/practice/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { usePosts } from "./hooks/usePosts";
+import "./App.css";
+
+const App = () => {
+  const [title, setTitle] = useState("");
+  const { posts, addPost, loading } = usePosts();     // 커스텀 훅 가져오기
+
+  const handleSubmit = () => {
+    addPost(title);
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <h2>게시물 목록</h2>
+      {loading ? (
+        <p>로딩 중</p>
+      ) : (
+        <ul>
+          {posts.map((p) => (
+            <li key={p.id}>{p.title}</li>
+          ))}
+        </ul>
+      )}
+
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button onClick={handleSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default App;
