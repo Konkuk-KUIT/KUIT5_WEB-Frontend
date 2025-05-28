7d92a673f320b5111cef03f7047ffe03004892c3
TakioN <jungbeen0724@gmail.com>
2025-05-29 04:55:38 +0900

    refactor: migrate JS to TS with week8

---
+++ week9/jungbeen/src/common/AddStoreBar.tsx
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+const AddForm = styled.form`
+  margin-bottom: 250px;
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+`;
+const AddInput = styled.input`
+  border: 1px solid black;
+  border-radius: 8px;
+  height: 50px;
+`;
+const FormButton = styled.button`
+  height: 50px;
+  font-size: 24px;
+  border-radius: 8px;
+  flex: 1;
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+type AddStoreBarProps = {
+  className?: string;
+  setIsadding: React.Dispatch<React.SetStateAction<boolean>>;
+  addStore: (
+    name: string,
+    rideTip: number,
+    paymethod: string,
+    minOrder: number
+  ) => Promise<void>;
+};
+
+function AddStoreBar({ className, setIsadding, addStore }: AddStoreBarProps) {
+  const [name, setName] = useState("");
+  const [rideTip, setRideTip] = useState("");
+  const [paymethod, setPaymethod] = useState("");
+  const [minOrder, setMinOrder] = useState("");
+
+  const handleAddButton = async () => {
+    if (name && rideTip && paymethod && minOrder) {
+      await addStore(name, Number(rideTip), paymethod, Number(minOrder));
+      setIsadding(false);
+    }
+  };
+
+  return (
+    <AddForm className={className}>
+      <AddInput
+        type="text"
+        placeholder="가게명"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <AddInput
+        type="number"
+        placeholder="배달비"
+        value={rideTip}
+        onChange={(e) => {
+          setRideTip(e.target.value);
+        }}
+      />
+      <AddInput
+        type="text"
+        placeholder="결제방법"
+        value={paymethod}
+        onChange={(e) => {
+          setPaymethod(e.target.value);
+        }}
+      />
+      <AddInput
+        type="number"
+        placeholder="최소 주문 금액"
+        value={minOrder}
+        onChange={(e) => {
+          setMinOrder(e.target.value);
+        }}
+      />
+      <ButtonWrapper style={{ display: "flex", gap: "5px" }}>
+        <FormButton type="button" onClick={handleAddButton}>
+          확인
+        </FormButton>
+        <FormButton
+          type="button"
+          onClick={async () => {
+            setIsadding(false);
+          }}
+        >
+          취소
+        </FormButton>
+      </ButtonWrapper>
+    </AddForm>
+  );
+}
+export default AddStoreBar;
---
+++ week9/jungbeen/src/pages/Router.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./Home";
+import Cart from "./Cart";
+import Store from "./Store";
+import Stores from "./Stores";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/store",
+      element: <Stores />,
+    },
+    {
+      path: "/store/:storeId",
+      element: <Store />,
+    },
+    {
+      path: "/cart",
+      element: <Cart />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
